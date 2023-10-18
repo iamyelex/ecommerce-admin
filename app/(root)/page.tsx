c80000adeb1ce86d80751e0b723b632cd978ff4d
iamyelex <yelex4love@gmail.com>
2023-10-18 07:47:27 -0700

    project setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <section className="p-4">
+      <p>Hello Admin Dashboard</p>
+      <Button variant="destructive">Click Me</Button>
+    </section>
+  );
+}
